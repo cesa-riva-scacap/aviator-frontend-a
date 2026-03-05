cd8194da6d3f9f56112a24ac7cccb7025f8674ba
rivacesar <cesar.riva@scalable.capital>
2026-03-05 15:33:19 +0100

    Add dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-function App() {
+import { Dashboard } from "./dashboard";
+
+const App = () => {
   return (
-    <main className="min-h-screen w-full">
+    <main className="min-h-screen w-full p-5">
       <div>
-        <h1 className="text-3xl">Aviator Frontend PoC - Market Data Display</h1>
+        <h1 className="text-3xl font-medium">
+          Aviator Frontend PoC - Market Making Workstation
+        </h1>
+        <Dashboard />
       </div>
     </main>
   );
-}
+};
 
 export default App;
